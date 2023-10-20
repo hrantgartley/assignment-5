4abc6dfcadc57d97c9b97a51ecbe71a75cc7aedb
hrantgartley <doyouevenjump@gmail.com>
2023-10-19 20:43:06 -0500

    validate html side with onsubmit inside of the form to prevent it from being submitted if false is returned

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ function validate() {
     const input = document.getElementById("meep").value
     if (input === null || input === "") {
         alert("PUT SOMETHING IN THE DAMN BOX")
+        window.location.href = "./index.php"
         return false
     }
+    return true
 }
