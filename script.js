841536909d9c477e71ca9a57c9c97213c0540fe2
hrantgartley <doyouevenjump@gmail.com>
2023-10-19 20:35:44 -0500

    js input validation

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const refreshPage = () => {
     location.reload()
 }
+
+function validate() {
+    const input = document.getElementById("meep").value
+    if (input === null || input === "") {
+        alert("PUT SOMETHING IN THE DAMN BOX")
+        return false
+    }
+}
--- show_form.php
+++ show_form.php
@@ -3,8 +3,8 @@
         <div class="reel" id="reel1">
             <div class="form">
                 <form method="POST" action="index.php">
-                    <p>Enter your bet (less than $1,000.01): <input type="number" name="bet" min="0.01" max="1000" step="0.01" required placeholder="nnnn.nn" autofocus></p>
-                    <button type="submit" name="play-button" id="play-button">Play</button>
+                    <p>Enter your bet (less than $1,000.01): <input type="number" name="bet" min="0.01" max="1000" step="0.01" placeholder="nnnn.nn" autofocus id="meep"></p>
+                    <button type="submit" name="play-button" id="play-button" onclick="validate()">Play</button>
                     <button type="button" onclick="refreshPage()">Refresh</button>
                 </form>
             </div>
