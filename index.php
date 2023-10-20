61d11e853a807fb336a965d94b3b17a66b44357d
Grant <grant@Grants-MacBook-Pro.local>
2023-10-19 20:47:53 -0500

    php validation added... not complete bc ugly but im done working on this forever

--- index.php
+++ index.php
@@ -2,25 +2,26 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Slot Machine Simulator</title>
-    <link rel="stylesheet" type="text/css" href="styles.css">
-    <link rel="icon" type="image/x-icon" href="./slotMachine.ico">
-    <script src="./script.js"></script>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Slot Machine Simulator</title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	<link rel="icon" type="image/x-icon" href="./slotMachine.ico">
+	<script src="./script.js"></script>
 
 </head>
 
 <body>
-    <?php
-    require('head.php');
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        require('process_form.php');
-    } else {
-        require('show_form.php');
-    }
-    require('foot.php');
-    ?>
+	<?php
+	require('head.php');
+	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+		require('process_form.php');
+	} else {
+		require('show_form.php');
+	}
+	require('./foot.php');
+	?>
+
 
 </body>
 
